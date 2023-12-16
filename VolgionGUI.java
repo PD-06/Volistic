310f7fed207edd166ec411ffe14d00bc29cc7515
DeffreusTheda <thedadeffreus@gmail.com>
2023-12-16 19:50:45 +0700

    Implemented First ActionListener

--- VolgionGUI.java
+++ VolgionGUI.java
@@ -3,22 +3,41 @@
  * define px pixel
  */
 
+import java.awt.event.*;
 import javax.swing.*;
 
 public class VolgionGUI {
-    JFrame frame; // Declare `frame`
+    JFrame frame; // Declare `frame`; here -> global
 
     VolgionGUI() {
-        frame = new JFrame(); // Create an instance of JFrame; initialize `frame`
+        // Create an instance of JFrame
+        frame = new JFrame("Welcome to VolgionGUI!");
 
-        JButton button = new JButton("I'm a button!"); // Create an istance of of JButton; declare & initialize
-                                                       // `button`
-        button.setBounds(100, 50, 100, 50); // Set respectively: x axis in px from upper-right corner, y axis in px
-                                            // from upper-right corner, button's width, and button's height
+        // Create an instance of JTextField (Where input usually goes)
+        JTextField textField = new JTextField();
+        // Refer to line 24-26 && TODO: UPDATE THIS COMMENT
+        textField.setBounds(50, 100, 200, 25);
 
-        frame.add(button); // Add `button` (a JButton) to `frame` (a JFrame)
+        // Create an istance of of JButton
+        JButton button = new JButton("I'm a button!");
 
-        frame.setSize(300, 180); // Widht, height: include window title bar (around 30px)
+        // Set respectively:
+        // x axis in px from upper-right corner, y axis in px rom upper-right corner,
+        // button's width, and button's height
+        button.setBounds(100, 50, 100, 50);
+        // Add an Action Listener (Trigger when the button is pressed)
+        button.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                textField.setText("You clicked the button! :O");
+            }
+        });
+
+        // Semantics
+        frame.add(button);
+        frame.add(textField);
+
+        // Widht, height: include window title bar (around 30px)
+        frame.setSize(300, 180);
         frame.setLayout(null); // Use no layout manager
         frame.setVisible(true); // Make the frame visible; default is false, I think
     }
