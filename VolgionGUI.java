a7034d856f9d264d25c4c42cc3d94244474b73a5
DeffreusTheda <thedadeffreus@gmail.com>
2023-12-16 20:40:01 +0700

    Added icon

--- VolgionGUI.java
+++ VolgionGUI.java
@@ -15,16 +15,21 @@ public class VolgionGUI {
 
         // Create an instance of JTextField (Where input usually goes)
         JTextField textField = new JTextField();
-        // Refer to line 24-26 && TODO: UPDATE THIS COMMENT
-        textField.setBounds(50, 100, 200, 25);
-
-        // Create an istance of of JButton
+        // Create an instance of ImageIcon
+        ImageIcon icon = new ImageIcon("GUIFiles/small_hello_silverwolf.png");
+        // Create istances of JButton
         JButton button = new JButton("I'm a button!");
+        JButton image = new JButton(icon);
 
         // Set respectively:
-        // x axis in px from upper-right corner, y axis in px rom upper-right corner,
-        // button's width, and button's height
-        button.setBounds(100, 50, 100, 50);
+        // x axis in px from JFrame's left border,
+        // y axis in px from JFrame's top border,
+        // width, and height
+        button.setBounds(300, 200, 200, 100);
+        textField.setBounds(300, 300, 200, 50);
+        // Set the dimensions based on the image size
+        image.setBounds(350, 50, 100, 100);
+
         // Add an Action Listener (Trigger when the button is pressed)
         button.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent e) {
@@ -34,12 +39,14 @@ public class VolgionGUI {
 
         // Semantics
         frame.add(button);
+        frame.add(image);
         frame.add(textField);
 
         // Widht, height: include window title bar (around 30px)
-        frame.setSize(300, 180);
+        frame.setSize(800, 500);
         frame.setLayout(null); // Use no layout manager
         frame.setVisible(true); // Make the frame visible; default is false, I think
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     }
 
     public static void main(String[] args) {
