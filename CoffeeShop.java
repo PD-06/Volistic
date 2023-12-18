0ac8160965313e6208fdb203a2034f6060b07b12
DeffreusTheda <thedadeffreus@gmail.com>
2023-12-19 02:07:05 +0700

    Huge Progress

--- CoffeeShop.java
+++ CoffeeShop.java
@@ -1,96 +1,170 @@
 import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-
+import java.awt.event.*;
 import javax.swing.*;
 
 public class CoffeeShop extends JFrame {
     private static String[] items = { "Americano ", "Latte     ", "Cappuccino", "Espresso  ", "Arabica   ",
             "Mochaccino", "Tiramisu  ", "Robusta   ", "Liberica  ", "Excelso   ", "Affogato  " };
-    /*
-     * private static int[] prices = { 21, 24, 29, 19, 23, 33, 33, 30, 66, 95, 34 };
-     * private static int[] orders = { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
-     */
+    private static int[] prices = { 21, 24, 29, 19, 23, 33, 33, 30, 66, 95, 34 };
+    private static int[] orders = { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
     final private Font mainFont = new Font("Monospace", Font.PLAIN, 12);
+    double cost;
+    boolean isDiscount;
+    JLabel lRawCost, lIsDiscount, lFinalCost;
+    JTextField[] tfCount;
 
     CoffeeShop() {
         JPanel pCF = new JPanel();
-        pCF.setLayout(new BorderLayout());
+        cost = 0;
+        isDiscount = false;
+        pCF.setLayout(new BorderLayout(20, 15));
 
-        // North
+        /******************************** NORTH *********************************/
         JLabel lWelcome = new JLabel("*If your purchase reach Rp.100K, you will get a 15% discount!");
         lWelcome.setFont(mainFont);
         JPanel pNorth = new JPanel();
         pNorth.add(lWelcome);
         pCF.add(pNorth, BorderLayout.NORTH);
 
-        // West
+        /********************************* WEST *********************************/
         JButton bPurchase = new JButton("Purchase");
         bPurchase.setFont(mainFont);
+        bPurchase.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                update();
+                // TODO: new Transaction();
+            }
+        });
         JButton bReset = new JButton("Reset");
         bReset.setFont(mainFont);
+        bReset.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                cost = 0;
+                for (int i = 0; i < items.length; i++) {
+                    orders[i] = 0;
+                    tfCount[i].setText("0");
+                }
+                update();
+            }
+        });
         JPanel pWest = new JPanel();
         pWest.add(bPurchase);
         pWest.add(bReset);
         pWest.setLayout(new GridLayout(2, 1));
         pCF.add(pWest, BorderLayout.WEST);
 
-        // Center
+        /******************************** CENTER ********************************/
         JLabel lItem;
-        JLabel lMenu = new JLabel("Available Menu:");
+        JLabel lPrice;
+        JLabel lItemList = new JLabel("Menu:");
+        JLabel lPriceList = new JLabel("Price:");
         JPanel pCenter = new JPanel();
-        pCenter.add(lMenu);
+        pCenter.add(lItemList);
+        pCenter.add(lPriceList);
         for (int i = 0; i < items.length; i++) {
             lItem = new JLabel(items[i]);
             pCenter.add(lItem);
+            lPrice = new JLabel("(" + prices[i] + "K)");
+            pCenter.add(lPrice);
         }
-        pCenter.setLayout(new GridLayout((items.length + 1), 1));
+        pCenter.setLayout(new GridLayout((items.length + 1), 2, 30, 0));
         pCF.add(pCenter, BorderLayout.CENTER);
 
-        // East
-        JTextField tfCount1 = new JTextField("0");
-        tfCount1.setSize(50, 50);
-        JButton min1 = new JButton("-");
-        min1.setSize(50, 50);
-        min1.addActionListener(new ActionListener() {
-            public void actionPerformed(ActionEvent e) {
-                tfCount1.setText(Integer.toString(Integer.parseInt(tfCount1.getText()) - 1));
-                if (Integer.parseInt(tfCount1.getText()) < 0) {
-                    tfCount1.setText("0");
+        /********************************* EAST *********************************/
+        JPanel pEast = new JPanel();
+        JLabel lOrder = new JLabel("Orders:");
+        pEast.add(lOrder);
+        JLabel lReduceButtons = new JLabel("");
+        pEast.add(lReduceButtons);
+        JLabel lIncreaseButtons = new JLabel("");
+        pEast.add(lIncreaseButtons);
+        tfCount = new JTextField[items.length];
+        JButton[] bReduce = new JButton[items.length];
+        JButton[] bIncrease = new JButton[items.length];
+        for (int i = 0; i < tfCount.length; i++) {
+            final int id = i;
+            tfCount[i] = new JTextField("0");
+            pEast.add(tfCount[i]);
+            bReduce[i] = new JButton("-");
+            bReduce[i].addActionListener(new ActionListener() {
+                public void actionPerformed(ActionEvent k) {
+                    tfCount[id].setText(Integer.toString(Integer.parseInt(tfCount[id].getText()) - 1));
+                    orders[id]--;
+                    if (Integer.parseInt(tfCount[id].getText()) < 0) {
+                        tfCount[id].setText("0");
+                        orders[id] = 0;
+                    }
+                    update();
                 }
-            }
-        });
-        JButton plus1 = new JButton("+");
-        plus1.setSize(50, 50);
-        plus1.addActionListener(new ActionListener() {
-            public void actionPerformed(ActionEvent e) {
-                tfCount1.setText(Integer.toString(Integer.parseInt(tfCount1.getText()) + 1));
-            }
-        });
-        JPanel pPlusOrMinus1 = new JPanel();
-        pPlusOrMinus1.add(min1);
-        pPlusOrMinus1.add(plus1);
-        pPlusOrMinus1.setLayout(new GridLayout());
-        JPanel pButtons1 = new JPanel();
-        pButtons1.add(tfCount1);
-        pButtons1.add(pPlusOrMinus1);
-        pButtons1.setLayout(new GridLayout(1, 2, 5, 0));
-        pCF.add(pButtons1, BorderLayout.EAST);
+            });
+            pEast.add(bReduce[i]);
+            bIncrease[i] = new JButton("+");
+            bIncrease[i].addActionListener(new ActionListener() {
+                public void actionPerformed(ActionEvent e) {
+                    tfCount[id].setText(Integer.toString(Integer.parseInt(tfCount[id].getText()) + 1));
+                    orders[id]++;
+                    update();
+                }
+            });
+            pEast.add(bIncrease[i]);
+        }
+        pEast.setLayout(new GridLayout((items.length + 1), 3));
+        pEast.setMaximumSize(new Dimension(225, 800));
+        pCF.add(pEast, BorderLayout.EAST);
 
-        // South
-        double cost = 0;
+        /******************************** SOUTH *********************************/
         JPanel pSouth = new JPanel();
-        JLabel lPrice = new JLabel("Price: Rp." + cost + "K");
-        pSouth.add(lPrice);
+        // Price
+        JLabel lTotalPrice = new JLabel("Total Price:");
+        pSouth.add(lTotalPrice);
+        lRawCost = new JLabel("Rp. " + nice(cost) + "K");
+        pSouth.add(lRawCost);
+        // Discount
+        JLabel lDiscountState = new JLabel("Discount:");
+        pSouth.add(lDiscountState);
+        String discount = (isDiscount) ? "15%" : "None";
+        lIsDiscount = new JLabel(discount);
+        pSouth.add(lIsDiscount);
+        // Final Cost
+        JLabel lFinalCostStatement = new JLabel("Final Cost:");
+        pSouth.add(lFinalCostStatement);
+        double finalCost = (isDiscount) ? cost * 0.85 : cost;
+        lFinalCost = new JLabel("Rp. " + nice(finalCost) + "K");
+        pSouth.add(lFinalCost);
+        // The rest
+        pSouth.setLayout(new GridLayout(3, 2));
         pCF.add(pSouth, BorderLayout.SOUTH);
 
         add(pCF);
-        setSize(500, 800);
+        setSize(600, 400);
         setTitle("Welcome to Volgion Coffee Shop!");
-        setMinimumSize(new Dimension(500, 800));
-        setMaximumSize(new Dimension(500, 800));
+        // setMinimumSize(new Dimension(500, 800));
+        // setMaximumSize(new Dimension(500, 800));
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setBackground(new Color(99, 97, 107));
         setVisible(true);
+    }
+
+    void update() {
+        cost = 0;
+        for (int i = 0; i < items.length; i++) {
+            cost += orders[i] * prices[i];
+        }
+        lRawCost.setText("Rp. " + nice(cost) + "K");
+        if (cost >= 100) {
+            isDiscount = true;
+        } else {
+            isDiscount = false;
+        }
+        String discount = (isDiscount) ? "15%" : "None";
+        lIsDiscount.setText(discount);
+        double finalCost = (isDiscount) ? cost * 0.85 : cost;
+        lFinalCost.setText("Rp. " + nice(finalCost) + "K");
+    }
+
+    String nice(double cc) {
+        String res = (cc % 1 == 0) ? String.format("%.0f", cc) : String.format("%.1f", cc);
+        return res;
     }
 
     public static void main(String[] args) {
