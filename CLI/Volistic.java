793b0a2386706d1025152cca35f7ac957b0255bc
DeffreusTheda <thedadeffreus@gmail.com>
2023-12-24 20:38:16 +0700

    Code polishing

--- CLI/Volistic.java
+++ CLI/Volistic.java
@@ -6,7 +6,7 @@ import java.util.*;
 /*
  * Hello friends!
  * Here, you can change the code however you want,
- * Lines that start with '#' is a command, press Ccommand + Shift + P adn then type the following command!
+ * Lines that start with '#' is a command, press Command + Shift + P adn then type the following command!
  * 
  * If you want to publish the changes you made:
  * # Git: Commit
@@ -17,58 +17,46 @@ import java.util.*;
  */
 
 public class Volistic {
+    static boolean run;
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
 
-        boolean run = true;
+        run = true;
         while (run) {
-            try {
-                Main.clear();
-                Main.pBanner();
-                Main.pQuit();
-                Main.pMainMenu();
-                System.out.println("\nSelect the program you want to run: ");
-                String chosenProgram = input.nextLine();
-                switch (chosenProgram) {
-                    case "Coffee Shop":
-                    case "1":
-                        CoffeeShop.run();
-                        break;
-                    case "BMI":
-                    case "BMI Calculator":
-                    case "2":
-                        BMICalculator.run();
-                        break;
-                    case "3":
-                        TwoVarCalculator.run();
-                        break;
-                    case "4":
-                    case "Tristan":
-                    case "Tristan's Sleep Duration Calculator":
-                        Tristan.run();
-                        break;
-                    case "5":
-                    case "Anton":
-                    case "Morse":
-                    case "morse":
-                        Anton.run();
-                        break;
-                    case "exit":
-                    case "quit":
-                        Main.quit(3);
-                        break;
-                }
-            } catch (Exception e) {
-                Main.pExcType(
-                        "Something went wrong. Please report immediately to: deffreus (Discord). Sorry for the inconvenience T-T");
-                if (input.hasNextLine()) {
-                    input.nextLine();
-                }
-                try {
-                    Thread.sleep(2000);
-                } catch (Exception ee) {
-                    Main.pExcType("Thread sleep error.");
-                }
+            Main.clear();
+            Main.pBanner();
+            Main.pQuit();
+            Main.pMainMenu();
+            System.out.print("\nSelect the program you want to run: ");
+            String chosenProgram = input.nextLine();
+            switch (chosenProgram) {
+                case "Coffee Shop":
+                case "1":
+                    CoffeeShop.run();
+                    break;
+                case "BMI":
+                case "BMI Calculator":
+                case "2":
+                    BMICalculator.run();
+                    break;
+                case "3":
+                    TwoVarCalculator.run();
+                    break;
+                case "4":
+                case "Tristan":
+                case "Tristan's Sleep Duration Calculator":
+                    Tristan.run();
+                    break;
+                case "5":
+                case "Anton":
+                case "Morse":
+                case "morse":
+                    Anton.run();
+                    break;
+                case "exit":
+                case "quit":
+                    Main.quit();
+                    break;
             }
         }
         input.close();
