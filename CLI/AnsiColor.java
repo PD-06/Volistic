332f50edde12414bf32e5ed57c867a54fb63aea3
DeffreusTheda <thedadeffreus@gmail.com>
2023-12-25 13:34:00 +0700

    Variables encapsulation

--- CLI/AnsiColor.java
+++ CLI/AnsiColor.java
@@ -1,12 +1,12 @@
 package CLI;
 
 public class AnsiColor {
-    static final String RESET = "\u001B[0m";
-    static final String RED = "\u001B[31m";
-    static final String GREEN = "\u001B[32m";
-    static final String YELLOW = "\u001B[33m";
-    static final String PURPLE = "\u001B[35m";
-    static final String CYAN = "\u001B[36m";
-    static final String WHITE = "\u001B[37m";
-    static String[] colors = {RED, GREEN, YELLOW, PURPLE, CYAN, WHITE};
+    protected static final String RESET = "\u001B[0m";
+    protected static final String RED = "\u001B[31m";
+    protected static final String GREEN = "\u001B[32m";
+    protected static final String YELLOW = "\u001B[33m";
+    protected static final String PURPLE = "\u001B[35m";
+    protected static final String CYAN = "\u001B[36m";
+    protected static final String WHITE = "\u001B[37m";
+    protected static final String[] colors = {RED, GREEN, YELLOW, PURPLE, CYAN, WHITE};
 }
