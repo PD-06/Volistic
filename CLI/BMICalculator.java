0e7e64e5ed7288e7e8dbe6ae1839157c64d53e53
DeffreusTheda <thedadeffreus@gmail.com>
2023-12-26 20:43:40 +0700

    Added license information on source code
     + Minor code formatting

--- CLI/BMICalculator.java
+++ CLI/BMICalculator.java
@@ -1,3 +1,25 @@
+/********************************************************************************
+ * Copyright (c) 2024 PD`06
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ *******************************************************************************/
+
 package CLI;
 
 import java.util.Scanner;
@@ -50,9 +72,9 @@ public class BMICalculator {
                         System.out.println("Something is wrong with your weight");
                         continue;
                     }
-                    if(weight > 635) System.out.println("I don't think you can be that fat.");
-                    else if(weight <= 0) System.out.println("What are you?");
-                    else if(weight < 0.25) System.out.println("Are you a toothpick?");
+                    if (weight > 635) System.out.println("I don't think you can be that fat.");
+                    else if (weight <= 0) System.out.println("What are you?");
+                    else if (weight < 0.25) System.out.println("Are you a toothpick?");
                     else inputIsValid = true;
                 }
             }
@@ -110,7 +132,7 @@ public class BMICalculator {
                             break;
                     }
                 }
-                if(end) pQuit();
+                if (end) pQuit();
             }
             // Print a 3-second quit timer
             static void pQuit() {
