2aaf6a87eef898c5f90355f4eba1028b99ae33c6
DeffreusTheda <thedadeffreus@gmail.com>
2023-12-26 22:16:57 +0700

    Update V1.1.0
    + Stricter time format
    + Updated question prompts
    + Improved quitting response vocab
    + Updated license information
    + Improved encapsulation
    + Major name style rework

--- CLI/Tristan.java
+++ CLI/Tristan.java
@@ -1,154 +1,188 @@
+/********************************************************************************
+ * Copyright (c) 2024 PD`06
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ *******************************************************************************/
+
 package CLI;
 
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Scanner;
 
 public class Tristan {
-            static Scanner tristaninput = new Scanner(System.in);
-            static boolean tristanIsRunning, tristanIsEnded;
-            static int jam_tidur, menit_tidur, jam_bangun, menit_bangun;
+    private static final Scanner scanner = new Scanner(System.in);
+    private static boolean end;
+    private static int sleepTime, wakeUpTime;
 
-            static void run() {
-                tristanIsRunning = true;
-                tristanIsEnded = false;
-                while (tristanIsRunning) {
-                    tristanIsRunning = false;
-                    try {
-                        Main.clear();
-                        pWelcome();
-                        iJamTidur();
-                        iJamBangun();
-                        pRes();
-                        iQuit();
-                        if (tristanIsEnded == false) {
-                            tristanIsRunning = true;
-                        }
-                    } catch (Exception e) {
-                        Main.pExcType(AnsiColor.RED + "CALCULATOR ERROR." + AnsiColor.RESET);
-                    }
-                }
+    static void run() {
+        boolean isRunning = true;
+        end = false;
+        while (isRunning) {
+            isRunning = false;
+            try {
+                Main.clear();
+                printWelcome();
+                inputSleepTime();
+                inputWakeUpTime();
+                printResult();
+                iQuit();
+                if (!end) isRunning = true;
+            } catch (Exception e) {System.out.printf("%sCalculator crashed.%s",AnsiColor.RED,AnsiColor.RESET);}
+        }
+    }
+    static void printWelcome() {
+        Main.hr();
+        System.out.println("\nWelcome to Tristan's Sleep Duration Calculator!\n");
+        System.out.println("Note: Please use the 24 hour format!\n");
+        Main.hr();
+    }
+    static void inputSleepTime() {
+        boolean inputIsValid = false;
+        int sleepHour, sleepMinute;
+        while (!inputIsValid) {
+            System.out.print("\nEnter the time when you start sleeping (HH:MM): ");
+            char[] input = Main.noSpace(scanner.nextLine()).toCharArray();
+            if (input.length!=5) {
+                printInvalidFormat();
+                continue;
             }
-            static void pWelcome() {
-                Main.hr();
-                System.out.println("\nWelcome to the Tristan's Sleep Duration Calculator!\n");
-                System.out.println("Write time in the 24 hour format!");
-                System.out.println("Write '24:00' as '00:00' !");
-                System.out.println("\n(Tristan gave the idea, and Deffreus made the code)\n");
-                Main.hr();
+            if (input[2]!=':') {
+                printInvalidFormat();
+                continue;
             }
-            static void iJamTidur() {
-                boolean inputIsValid = false;
-                while (!inputIsValid) {
-                    List<String> waktuTidur = new ArrayList<>();
-                    try {
-                        inputIsValid = true;
-                        System.out.println("\nAt what time did you slept? (e.g 22:15): ");
-                        String[] rawWaktuTidur = tristaninput.nextLine().split(":", 0);
-                        for(String i: rawWaktuTidur) {waktuTidur.add(i);}
-                        jam_tidur = Integer.parseInt(waktuTidur.get(0));
-                        menit_tidur = Integer.parseInt(waktuTidur.get(1));
-                        // check validity:
-                        if((jam_tidur >= 24 || jam_tidur < 0) || (menit_tidur < 0 || menit_tidur >= 60) || rawWaktuTidur.length > 2) {
-                            inputIsValid = false;
-                            Main.pExcType("That's not a valid time!");
-                        }
-                    } catch (Exception e) {
-                        Main.pExcType("Please enter the hour and minutes in a valid format: HH:MM");
-                        inputIsValid = false;
-                        waktuTidur.clear();
-                    }
-                }
+            try {
+                sleepHour = Integer.parseInt(String.valueOf(input[0])) * 10 + Integer.parseInt(String.valueOf(input[1]));
+            } catch (Exception e) {
+                printInvalidFormat();
+                continue;
             }
-            static void iJamBangun() {
-                boolean inputIsValid = false;
-                while (!inputIsValid) {
-                    List<String> waktuBangun = new ArrayList<>();
-                    try {
-                        inputIsValid = true;
-                        System.out.println("\nAt what time did you wake up? (e.g 03:50): ");
-                        String[] rawWaktuBangun = tristaninput.nextLine().split(":", 0);
-                        for(String i: rawWaktuBangun) {waktuBangun.add(i);}
-                        jam_bangun = Integer.parseInt(waktuBangun.get(0));
-                        menit_bangun = Integer.parseInt(waktuBangun.get(1));
-                        // check validity:
-                        if((jam_bangun >= 24 || jam_bangun < 0) || (menit_bangun < 0 || menit_bangun >= 60) || rawWaktuBangun.length > 2) {
-                            inputIsValid = false; Main.pExcType("That's not a valid time!");
-                        }
-                    } catch (Exception e) {
-                        Main.pExcType("Please enter the hour and minutes in a valid format: HH:MM");
-                        inputIsValid = false;
-                        waktuBangun.clear();
-                    }
-                }
+            if (sleepHour>24) {
+                printInvalidTime();
+                continue;
             }
-            static void pRes() {
-                // kalkulasi durasi tidur:
-                int tidur = jam_tidur*60+menit_tidur; int bangun = jam_bangun*60+menit_bangun;
-                if(tidur > bangun) {tidur -= (24*60);}
-                int hours = (bangun-tidur)/60; int minutes = (bangun-tidur)%60;
-
-                // output formatting!
-                // ketika tidur sama dengan bangun:
-                if(hours+minutes == 0) {System.out.printf("\nYou sleep in less than a minute or what? :O\n");}
-                // ketika tidur dibawah satu jam:
-                else if(hours == 0 && minutes != 0) {
-                    if(minutes == 1) {System.out.printf("\nYou've slept for a minute! XD\n");}
-                    else {System.out.printf("\nYou've slept for %d minutes! :v\n", minutes);}
-                }
-                // ketika tidur tepat N jam:
-                else if(minutes == 0 && hours != 0) {
-                    if(hours == 1) System.out.printf("\nYou've slept for an hour! :p\n");
-                    else {System.out.printf("\nYou've slept for %d hours! :>\n", hours);}
-                } 
-                // output format jam dan menit:
-                else {
-                    if(hours == 1 && minutes == 1) {System.out.printf("\nYou've slept for an hour and a minute! o_o\n");}
-                    else if(hours == 1) {System.out.printf("\nYou've slept for an hours and %d minutes! :)\n", minutes);}
-                    else if(minutes == 1) {System.out.printf("\nYou've slept for %d hours and a minute! c:\n", hours);}
-                    else System.out.printf("\nYou slept for " + hours + " hours and " + minutes + " minutes! :D\n");
-                }
+            try {
+                sleepMinute = Integer.parseInt(String.valueOf(input[3])) * 10 + Integer.parseInt(String.valueOf(input[4]));
+            } catch (Exception e) {
+                printInvalidFormat();
+                continue;
             }
-            static void iQuit() {
-                String opt;
-                boolean valid = false;
-                while (valid != true) {
-                    System.out.println("\nDo you want calculate again? (Y/n)");
-                    opt = tristaninput.nextLine();
-                    switch (opt) {
-                        case "Y":
-                        case "y":
-                        case "Yes":
-                        case "yes":
-                        case "YES":
-                        case "":
-                            valid = true;
-                            tristanIsEnded = false;
-                            break;
-                        case "n":
-                        case "N":
-                        case "No":
-                        case "no":
-                        case "NO":
-                            valid = true;
-                            tristanIsEnded = true;
-                            System.out.println("\nThank you for using the Sleep Duration Calculator!! :D");
-                            pQuit(3);
-                            break;
-                        default:
-                            System.out.println("Invalid input!");
-                    }
-                }
+            if (sleepMinute>=60) {
+                printInvalidTime();
+                continue;
             }
-            static void pQuit(int second) {
-                System.out.println("\nQuitting in:");
-                for(int i = second; i > 0; i--) {
-                    System.out.println(i + "...");
-                    try {
-                        Thread.sleep(1000);
-                    } catch(Exception e) {
-                        Main.pExcType("Thread sleep exception! Dev's fault, not yours!");
-                    }
-                }
+            sleepTime = sleepHour * 60 + sleepMinute;
+            if (sleepTime>1440) {
+                printInvalidTime();
+                continue;
+            }
+            inputIsValid = true;
+        }
+    }
+    static void printInvalidFormat() {Main.pEcxInvalI("Not in format 'HH:MM'");}
+    static void printInvalidTime() {Main.pEcxInvalI("Invalid time");}
+    static void inputWakeUpTime() {
+        boolean inputIsValid = false;
+        int wakeUpHour, wakeUpMinute;
+        while (!inputIsValid) {
+            System.out.print("\nEnter the time when you wake up (HH:MM): ");
+            char[] input = Main.noSpace(scanner.nextLine()).toCharArray();
+            if (input.length!=5) {
+                printInvalidFormat();
+                continue;
+            }
+            if (input[2]!=':') {
+                printInvalidFormat();
+                continue;
+            }
+            try {wakeUpHour = Integer.parseInt(String.valueOf(input[0])) * 10 + Integer.parseInt(String.valueOf(input[1]));}
+            catch (Exception e) {printInvalidFormat(); continue;}
+            if (wakeUpHour>24) {
+                printInvalidTime();
+                continue;
+            }
+            try {wakeUpMinute = Integer.parseInt(String.valueOf(input[3])) * 10 + Integer.parseInt(String.valueOf(input[4]));}
+            catch (Exception e) {
+                printInvalidFormat();
+                continue;
+            }
+            if (wakeUpMinute>=60) {
+                printInvalidTime();
+                continue;
+            }
+            wakeUpTime = wakeUpHour * 60 + wakeUpMinute;
+            if (wakeUpTime>1440) {
+                printInvalidTime();
+                continue;
+            }
+            inputIsValid = true;
+        }
+    }
+    static void printResult() {
+        if (sleepTime > wakeUpTime) wakeUpTime += (24*60);
+        int durationHour = (wakeUpTime-sleepTime)/60;
+        int durationMinute = (wakeUpTime-sleepTime)%60;
+        if (durationHour+durationMinute == 0) {System.out.print("\nYou sleep in less than a minute or what? :O\n");}
+        else if (durationHour == 0 && durationMinute != 0) {
+            if (durationMinute == 1) {System.out.print("\nYou've slept for a minute! XD\n");}
+            else {System.out.printf("\nYou've slept for %d minutes! :v\n", durationMinute);}
+        }
+        else if (durationHour != 0 && durationMinute == 0) {
+            if (durationHour == 1) System.out.print("\nYou've slept for an hour! :p\n");
+            else {System.out.printf("\nYou've slept for %d hours! :>\n", durationHour);}
+        }
+        else {
+            if (durationHour == 1 && durationMinute == 1) {System.out.print("\nYou've slept for an hour and a minute! o_o\n");}
+            else if (durationHour == 1) {System.out.printf("\nYou've slept for an hour and %d minutes! :)\n", durationMinute);}
+            else if (durationMinute == 1) {System.out.printf("\nYou've slept for %d hours and a minute! c:\n", durationHour);}
+            else System.out.printf("\nYou slept for %s hours and %d minutes! :D\n", durationHour, durationMinute);
+        }
+    }
+    static void iQuit() {
+        String opt;
+        boolean valid = false;
+        while (!valid) {
+            System.out.println("\nDo you want calculate again? (Y/n)");
+            opt = scanner.nextLine().trim().toLowerCase();
+            valid = true;
+            switch (opt) {
+                case "y", "yes", "yeah", "yup", "yoi", "yep", "aye", "ye", "":
+                    end = false;
+                    break;
+                case "n", "no", "nah", "nope", "nay":
+                    end = true;
+                    System.out.println("\nThank you for using the Sleep Duration Calculator!! :D");
+                    pQuit();
+                    break;
+                default:
+                    Main.pEcxInvalI("Response unknown.");
+                    valid = false;
             }
         }
+    }
+    static void pQuit() {
+        System.out.println("\nQuitting in:");
+        for (int i = 3; i > 0; i--) {
+            System.out.println(i + "...");
+            try {
+                Thread.sleep(1000);
+            } catch(Exception e) {
+                Main.printRed("Thread sleep exception found");
+            }
+        }
+    }
+}
