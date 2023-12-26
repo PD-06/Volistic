fd2389886bb5fab03c86a71874b24f8ec616bba7
DeffreusTheda <thedadeffreus@gmail.com>
2023-12-27 00:54:48 +0700

    Code polishing

--- CLI/Volistic.java
+++ CLI/Volistic.java
@@ -24,61 +24,26 @@ package CLI;
 
 import java.util.*;
 
-// Let start over again:
-/*
- * Hello friends!
- * Here, you can change the code however you want,
- * Lines that start with '#' is a command, press Command + Shift + P adn then type the following command!
- * 
- * If you want to publish the changes you made:
- * # Git: Commit
- * # Git: Push
- * 
- * Also, don't forget to Fetch frequently
- * # Git: Fetch
- */
-
 public class Volistic {
     static boolean run;
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-
         run = true;
+
         while (run) {
             Main.clear();
             Main.pBanner();
             Main.pQuit();
             Main.pMainMenu();
-            System.out.print("\nSelect the program you want to run: ");
-            String chosenProgram = input.nextLine();
+            System.out.print("\nEnter the ID of the program that you want to run: ");
+            String chosenProgram = input.nextLine().trim().toLowerCase();
             switch (chosenProgram) {
-                case "Coffee Shop":
-                case "1":
-                    CoffeeShop.run();
-                    break;
-                case "BMI":
-                case "BMI Calculator":
-                case "2":
-                    BMICalculator.run();
-                    break;
-                case "3":
-                    TwoVarCalculator.run();
-                    break;
-                case "4":
-                case "Tristan":
-                case "Tristan's Sleep Duration Calculator":
-                    Tristan.run();
-                    break;
-                case "5":
-                case "Anton":
-                case "Morse":
-                case "morse":
-                    Anton.run();
-                    break;
-                case "exit":
-                case "quit":
-                    Main.quit();
-                    break;
+                case "coffee shop", "1", "coffee": CoffeeShop.run(); break;
+                case "bmi", "bmi calculator", "2": BMICalculator.run(); break;
+                case "3", "tvc", "two variable calculator": TwoVarCalculator.run(); break;
+                case "4", "tristan", "sleep duration calculator": Tristan.run(); break;
+                case "5", "anton", "morse": Anton.run(); break;
+                case "exit", "quit", "q": Main.quit();
             }
         }
         input.close();
