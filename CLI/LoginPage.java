8be28c302782f313552e00d1f4c2f7f655f4a1cb
DeffreusTheda <thedadeffreus@gmail.com>
2023-12-31 05:43:13 +0700

    Added license information to source code
    + Code reformatting

--- CLI/LoginPage.java
+++ CLI/LoginPage.java
@@ -1,12 +1,31 @@
+/********************************************************************************
+ * Copyright (c) 2024 PD`06
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ *******************************************************************************/
+
 package CLI;
 
 import java.awt.BorderLayout;
 import java.awt.Dimension;
 import java.awt.FlowLayout;
 import java.awt.GridLayout;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
@@ -22,7 +41,7 @@ public class LoginPage extends JFrame {
 
     LoginPage() {
         setLayout(new BorderLayout());
-        /********************* North *********************/
+        /* ******************** North ******************** */
         JLabel lWelcome = new JLabel("Welcome to Volistic! Please login to continue.");
         JPanel pWelcome = new JPanel();
         pWelcome.add(lWelcome);
@@ -37,11 +56,11 @@ public class LoginPage extends JFrame {
         pNorth.setLayout(new GridLayout(2, 1));
         add(pNorth, BorderLayout.NORTH);
 
-        /********************* West *********************/
+        /* ******************** West ******************** */
         JPanel pWest = new JPanel();
         add(pWest, BorderLayout.WEST);
         
-        /********************* Center *********************/
+        /* ******************** Center ******************** */
         JLabel lUsername = new JLabel("Username:");
         JTextField tfUsername = new JTextField("");
         JLabel lPassword = new JLabel("Password:");
@@ -54,36 +73,23 @@ public class LoginPage extends JFrame {
         pCenter.setLayout(new GridLayout(4, 1));
         add(pCenter, BorderLayout.CENTER);
 
-        /********************* East *********************/
+        /* ******************** East ******************** */
         JPanel pEast = new JPanel();
         add(pEast, BorderLayout.EAST);
 
-        /********************* South *********************/
+        /* ******************** South ******************** */
         JLabel lStatus = new JLabel("");
         JPanel pStatus = new JPanel();
         pStatus.add(lStatus);
-        pStatus.setLayout(new FlowLayout(1));
-        JButton bLogin = new JButton("Login");
-        bLogin.addActionListener(new ActionListener() {
-            public void actionPerformed(ActionEvent e) {
-                authenticationSuccessful = false;
-                for(int i = 0; i < account[0].length; i++) {if(account[0][i].equals(tfUsername.getText()) && account[1][i].equals(tfPassword.getText())) {
-                    authenticationSuccessful = true;
-                    identity[0] = tfUsername.getText();
-                    if(identity[0].equals("")) {identity[0] = "Guest";}
-                    identity[1] = tfPassword.getText();
-                }}
-                if(authenticationSuccessful) {lStatus.setText("Login successful!");}
-                else {lStatus.setText("Login failed. Please try again.");}
-            }
-        });
+        pStatus.setLayout(new FlowLayout(FlowLayout.CENTER));
+        JButton bLogin = getBLogin(tfUsername, tfPassword, lStatus);
         JPanel pSouth = new JPanel();
         pSouth.add(pStatus);
         pSouth.add(bLogin);
         pSouth.setLayout(new GridLayout(2, 1));
         add(pSouth, BorderLayout.SOUTH);
 
-        /********************* Login Frame *********************/
+        /* ******************** Login Frame ******************** */
         
         setTitle("Volistic Login Page");
         setSize(300, 250);
@@ -91,7 +97,26 @@ public class LoginPage extends JFrame {
         setMinimumSize(new Dimension(300, 250));
         setVisible(true);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-    }   
+    }
+    
+    private JButton getBLogin(JTextField tfUsername, JTextField tfPassword, JLabel lStatus) {
+        JButton bLogin = new JButton("Login");
+        bLogin.addActionListener(e -> {
+            authenticationSuccessful = false;
+            for(int i = 0; i < account[0].length; i++) {if(account[0][i].equals(tfUsername.getText()) && account[1][i].equals(tfPassword.getText())) {
+                authenticationSuccessful = true;
+                identity[0] = tfUsername.getText();
+                if(identity[0].isEmpty()) {identity[0] = "Guest";}
+                identity[1] = tfPassword.getText();
+            }}
+            if(authenticationSuccessful) {
+                lStatus.setText("Login successful!");}
+            else {
+                lStatus.setText("Login failed. Please try again.");}
+        });
+        return bLogin;
+    }
+    
     public static void main(String[] args) {
         new LoginPage();
     }
